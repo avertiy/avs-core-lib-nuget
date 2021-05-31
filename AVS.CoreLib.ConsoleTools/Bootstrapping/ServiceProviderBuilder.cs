49817883a32b5d5f97ba5e7db9807c32db9cdfa7
Artem Vertiy <artem.vertiy@zip.co>
2021-05-31 10:40:29 +0300

    +startup base

--- AVS.CoreLib.ConsoleTools/Bootstrapping/ServiceProviderBuilder.cs
+++ AVS.CoreLib.ConsoleTools/Bootstrapping/ServiceProviderBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-//using AVS.CoreLib.ConsoleTools.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
---
+++ AVS.CoreLib.ConsoleTools/Bootstrapping/StartupBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using AVS.CoreLib.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AVS.CoreLib.ConsoleTools.Bootstrapping
+{
+    public abstract class StartupBase : IStartup
+    {
+        protected virtual string ContentRootPath => AppContext.BaseDirectory;
+        private IConfiguration _configuration;
+
+        protected IConfiguration Configuration => _configuration ??= LoadAppSettings();
+
+        public virtual void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            AddOptions(services);
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(Configuration.GetSection("Logging"));
+                ConfigureLogging(builder);
+            });
+
+            // coravel things
+            AddInvocables(services);
+            AddEventConsumers(services);
+        }
+
+        protected virtual void AddOptions(IServiceCollection services)
+        {
+            services.AddOptions();
+        }
+
+
+        protected void AddOptions<TOptions>(IServiceCollection services, string configSectionKey) where TOptions : class
+        {
+            var section = Configuration.GetSection(configSectionKey);
+            services.Configure<TOptions>(section.Bind);
+        }
+
+        protected virtual void AddEventConsumers(IServiceCollection services)
+        {
+            //services.AddScoped<EventHandler>(); // handler implement IListener<TEvent>
+        }
+
+        protected virtual void AddInvocables(IServiceCollection services)
+        {
+            //services.AddScoped<IInvocable>();
+        }
+
+        protected virtual void ConfigureLogging(ILoggingBuilder builder)
+        {
+        }
+
+        protected virtual IConfiguration LoadAppSettings(string appSettings = "appsettings.json")
+        {
+            var configurationBuilder = new ConfigurationBuilder().AddInMemoryCollection();
+            configurationBuilder.AddJsonFile(appSettings, true, true);
+            configurationBuilder.AddEnvironmentVariables();
+            return configurationBuilder.Build();
+        }
+
+        public virtual void ConfigureServices(IServiceProvider services)
+        {
+
+        }
+
+    }
+}
