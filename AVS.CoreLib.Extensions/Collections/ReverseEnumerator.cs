5fe1fa6b4379a09eeabe52c6ad5ecdd39aa19757
Artem Vertii <Artem.Vertii@global-e.com>
2024-02-20 00:04:28 +0200

    ReverseEnumerator

---
+++ AVS.CoreLib.Extensions/Collections/ReverseEnumerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AVS.CoreLib.Collections;
+
+public class ReverseEnumerator<TItem> : IEnumerator<TItem>
+{
+    private readonly List<TItem> _items;
+    private int _currentIndex;
+
+    public ReverseEnumerator(List<TItem> items)
+    {
+        _items = items;
+        Reset();
+    }
+
+    public TItem Current => _items[_currentIndex];
+
+    object IEnumerator.Current => Current!;
+
+    public void Dispose()
+    {
+        _items.Clear();
+    }
+
+    public bool MoveNext()
+    {
+        return --_currentIndex >= 0;
+    }
+
+    public void Reset()
+    {
+        _currentIndex = _items.Count;
+    }
+}
