29d12808bdec1f28475293de3cd3a2585bd8caa3
Artem Vertii <artem.vertii@global-e.com>
2024-03-10 11:42:23 +0200

    Deserialize extensions

--- AVS.CoreLib/Json/JsonExtensions.cs
+++ AVS.CoreLib/Json/JsonExtensions.cs
@@ -1,5 +1,7 @@
 ﻿#nullable enable
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using AVS.CoreLib.Dates;
 using System.Text.Json.Serialization;
@@ -11,21 +13,30 @@ namespace AVS.CoreLib.Json
     /// </summary>
     public static class JSON
     {
-        public static JsonSerializerOptions? DefaultOptions { get; set; }
+        private static JsonSerializerOptions? _options;
+        public static JsonSerializerOptions DefaultOptions => _options ??= Setup();
 
         /// <summary>
         /// Setup <see cref="DefaultOptions"/> 
-        /// (i) I prefer to have by default enum string values converter, PropertyNameCaseInsensitive and AllowTrailingCommas
-        /// (ii) add core lib converters such as <see cref="DateRangeJsonConverter"/>
+        /// (i) JsonSerializerDefaults.Web + AllowTrailingCommas
+        /// (ii) Converters: <see cref="JsonStringEnumConverter"/>, <see cref="DateRangeJsonConverter"/>
         /// </summary>
-        public static void Setup(Action<JsonSerializerOptions>? setup = null)
+        public static JsonSerializerOptions Setup(Action<JsonSerializerOptions>? setup = null)
         {
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, AllowTrailingCommas = true };
-            options.Converters.Add(new JsonStringEnumConverter());
-            options.Converters.Add(new DateRangeJsonConverter());
-
-            setup?.Invoke(options);
-            DefaultOptions = options;
+            if (_options == null)
+            {
+                _options = new JsonSerializerOptions
+                {
+                    NumberHandling = JsonNumberHandling.AllowReadingFromString,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    PropertyNameCaseInsensitive = true,
+                    AllowTrailingCommas = true
+                };
+                _options.Converters.Add(new JsonStringEnumConverter());
+                _options.Converters.Add(new DateRangeJsonConverter());
+            }
+            setup?.Invoke(_options);
+            return _options;
         }
 
         public static string ToJson(this object obj, JsonSerializerOptions? options = null)
@@ -46,9 +57,58 @@ namespace AVS.CoreLib.Json
             }
         }
 
+        #region Deserialize extensions
         public static T? Deserialize<T>(this string json, JsonSerializerOptions? options = null) where T : new()
         {
             return JsonSerializer.Deserialize<T>(json, options ?? DefaultOptions);
         }
+
+        public static T? Deserialize<T, TType>(this string json, JsonSerializerOptions? options = null) where TType : class, T
+        {
+            return (T?)JsonSerializer.Deserialize<TType>(json, options ?? DefaultOptions);
+        }
+
+        /// <summary>
+        /// <code>
+        ///     List{IBar} = json.DeserializeList{IBar,Bar}()
+        /// </code>
+        /// </summary>
+        public static List<T>? DeserializeList<T, TType>(this string json, JsonSerializerOptions? options = null) where TType : class, T
+        {
+            var list = JsonSerializer.Deserialize<List<TType>>(json, options ?? DefaultOptions);
+            return list?.Select(x => (T)x).ToList();
+        }
+
+        /// <summary>
+        /// <code>
+        ///     List{Bar} = json.DeserializeList{Bar}()
+        /// </code>
+        /// </summary>
+        public static List<T>? DeserializeList<T>(this string json, JsonSerializerOptions? options = null) where T : new()
+        {
+            return JsonSerializer.Deserialize<List<T>>(json, options ?? DefaultOptions);
+        }
+
+        /// <summary>
+        /// <code>
+        ///     Dictionary{string,IBar} = json.DeserializeDict{IBar,Bar}()
+        /// </code>
+        /// </summary>
+        public static Dictionary<string, T>? DeserializeDict<T, TType>(this string json, JsonSerializerOptions? options = null) where TType : class, T
+        {
+            var dict = JsonSerializer.Deserialize<Dictionary<string, TType>>(json, options ?? DefaultOptions);
+            return dict?.ToDictionary(x => x.Key, v => (T)v.Value);
+        }
+
+        /// <summary>
+        /// <code>
+        ///     Dictionary{string,Bar} = json.DeserializeDict{Bar}()
+        /// </code>
+        /// </summary>
+        public static Dictionary<string, T>? DeserializeDict<T>(this string json, JsonSerializerOptions? options = null) where T : new()
+        {
+            return JsonSerializer.Deserialize<Dictionary<string, T>>(json, options ?? DefaultOptions);
+        } 
+        #endregion
     }
 }
